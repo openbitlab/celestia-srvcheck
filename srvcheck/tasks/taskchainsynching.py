432b893e014780fd7b7556342f20c7901d23e85f
Lu191 <gigiamendolagine.la@gmail.com>
2023-04-06 13:20:43 +0200

    Fix sync output

--- srvcheck/tasks/taskchainsynching.py
+++ srvcheck/tasks/taskchainsynching.py
@@ -1,20 +1,27 @@
 from ..notification import Emoji
 from . import Task, minutes
 
-
 class TaskChainSynching(Task):
 	def __init__(self, services):
-		super().__init__('TaskChainSynching', services, minutes(1), minutes(30))
-		self.prev = None
+		super().__init__('TaskChainSynching', services, minutes(5), minutes(5))
+		self.prev = False
 
 	@staticmethod
 	def isPluggable(services):
 		return True
 
 	def run(self):
-		if self.s.chain.isSynching() and self.s.chain.TYPE == 'Validator node':
-			return self.notify(f'chain is synching {Emoji.Slow}')
-		elif self.s.chain.isSynching():
-			return self.notify(f'is synching data availability samples {Emoji.Slow}')
+		if self.s.chain.isSynching():
+			self.prev = True
+			if self.s.chain.TYPE == 'Validator node':
+				return self.notify(f'chain is synching {Emoji.Slow}')
+			else:
+				return self.notify(f'is synching data availability samples {Emoji.Slow}')
+		elif self.prev:
+			self.prev = False
+			if self.s.chain.TYPE == 'Validator node':
+				return self.notify(f'chain synched {Emoji.SyncOk}')
+			else:
+				return self.notify(f'synched all data availability samples {Emoji.SyncOk}')
 
 		return False
